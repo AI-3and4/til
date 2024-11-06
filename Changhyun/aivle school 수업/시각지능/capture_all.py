e8a9480fce44152a0275cb0db3fd66813942af5f
spaceOfSoul <ychh1123@gmail.com>
2024-11-06 21:36:23 +0900

    인공지능 정리

--- Changhyun/aivle school 수업/시각지능/capture_all.py
+++ Changhyun/aivle school 수업/시각지능/capture_all.py
@@ -1,22 +1,14 @@
-###############################################################
-## 해당 파일은 로컬 cam을 이용하여 본인의 이미지를 촬영하고 저장합니다.
-###############################################################
-
 import os
 import cv2
 
-## 이미지를 저장할 폴더 경로
 folder_path = '10_31'
-## 이미지 저장 폴더가 없다면 폴더 생성
 if not os.path.exists(folder_path) :
     os.makedirs(folder_path)
-    print(f'{folder_path} 폴더를 생성합니다.')
+    print(f'Make dir : {folder_path}')
 
-def capture_owner_images() : ## num_images에 숫자를 입력한만큼 이미지 저장
-    ## 0은 기본 웹캠
+def capture_owner_images() :
     cap = cv2.VideoCapture(0)
 
-    ## 위에서 지정한 수만큼 촬영 및 저장하는 반복문
     count = 0
     while True :
         _, frame = cap.read()
